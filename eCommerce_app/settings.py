c6a32e0d055545423997bc205a35ed6ff3787aa6
suvarna <suvrana0318@gmail.com>
2020-10-10 01:23:38 +0530

    static root updated

--- eCommerce_app/settings.py
+++ eCommerce_app/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,staticfiles)
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
 LOGIN_REDIRECT_URL='/products/home/'
