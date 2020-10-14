fe796a96e115e84855888d0d0f0abc4ca3ef738e
suvarna <suvrana0318@gmail.com>
2020-10-14 11:12:26 +0530

    here some err correction

--- eCommerce_app/settings.py
+++ eCommerce_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x$x(e##_2_kraltt5_==bw_@$=nbho$_oa9#*dhk6ibio=vuvz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
