6d11d326850a4d72c037576e16d668786e5ea5c2
suvarna <suvrana0318@gmail.com>
2020-10-27 00:06:10 +0530

    aws settings deleted

--- eCommerce_app/settings.py
+++ eCommerce_app/settings.py
@@ -146,15 +146,6 @@ EMAIL_PORT=587
 EMAIL_HOST_USER=os.environ.get('db_user')
 EMAIL_HOST_PASSWORD=os.environ.get('db_pass')
 
-AWS_ACCESS_KEY_ID='AKIAVTQAHAZEM5HZFSMP'
-AWS_SECRET_ACCESS_KEY='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'
-AWS_STORAGE_BUCKET_NAME='suvarna-bucket'
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME +'.s3.amazonaws.com'
 
-
-
-AWS_S3_FILE_OVERWRITE=False
-AWS_DEFAULT_ACL=None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
